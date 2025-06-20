47b5db3152bc04ee5e2ddfec20e3e68f2abd511c
sophiaamr <100172972+sophiaamr@users.noreply.github.com>
2025-06-20 20:08:09 -0300

    b

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,18 @@ import { ConfigModule } from '@nestjs/config';
 import { AvisoModule } from './modules/aviso/aviso.module';
 import { ManutencaoModule } from './modules/manutencao/manutencao.module';
 import { RegistroOcorrenciaModule } from './modules/registro_ocorrencia/registro_ocorrencia.module';
-import { AreasModule } from '@modules/areas/areas.module';
-import { BookingsModule } from '@modules/bookings/bookings.module';
-import { ApartamentoModule } from '@modules/apartamento/apartamento.module';
-import { AuthModule } from '@modules/auth/auth.module';
-import { UsersModule } from '@modules/users/users.module';
+import { AreasModule } from './modules/areas/areas.module';
+import { BookingsModule } from './modules/bookings/bookings.module';
+import { ApartamentoModule } from './modules/apartamento/apartamento.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 import { PackageModule } from './modules/package/package.module';
 import { VagaModule } from './modules/vaga/vaga.module';
 import { VisitanteModule } from './modules/visitante/visitante.module';
-import { ChatPrivadoModule } from '@modules/chat-privado/chat-privado.module';
+import { ChatPrivadoModule } from './modules/chat-privado/chat-privado.module';
 import { ChatGeralModule } from './modules/chat-geral/chat-geral.module';
 import { ProfissionalModule } from './modules/profissional/profissional.module';
-import { NotificationModule } from '@modules/notification/notification.module';
+import { NotificationModule } from './modules/notification/notification.module';
 
 @Module({
   imports: [
